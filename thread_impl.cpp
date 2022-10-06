f7633613dd3d5a4bd0a892239efdd050f08ec23f
ohjun <ohjun@umich.edu>
2022-10-06 14:08:14 -0400

    minor

--- thread_impl.cpp
+++ thread_impl.cpp
@@ -1,22 +1,5 @@
 #include "thread_impl.h"
 
-// Wrapper for thread function to track completion of stream of execution
-void thread_wrapper(/* thread_function */) {
-    // call thread_function
-    // call thread_exit
-}
-
-// Called after completion of stream of execution
-void thread_exit() {
-    // disable interrupts
-    // clear cpu finished_queue (no need to delete explicitly due to smart pointers)
-    // add current context to finished_queue
-    // if waiting_thread exists for this thread:
-        // remove waiting_thread from cpu waiting_set
-        // add waiting_thread to cpu ready_queue
-    // enable interrupts
-}
-
 thread::impl::impl() {
     // add thread context to cpu ready_queue
 }
